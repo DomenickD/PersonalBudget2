bce3c749744c24cf8c45ad78ce5570ddc3750b8b
DomenickD <deothe1@gmail.com>
2022-05-11 14:26:45 -0400

    got server to read html

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-app.get('/', (rew, res, next) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join( __dirname, './index.html' ));
-    // res.writeHead('Testing');
-    console.log('working');
+    // res.send('<h1>Hello from your Express.js server!!</h1>');
+    // console.log('working');
 })
 
 app.listen(PORT, () => {
