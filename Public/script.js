afaa249eb3b6c833c0c7a33b10c0f4478e92f934
DomenickD <deothe1@gmail.com>
2022-05-11 23:09:34 -0400

    JS working

---
+++ Public/script.js
@@ -0,0 +1,23 @@
+// to add logic
+
+const demo = document.getElementById("demo");
+// .innerHTML = <p>Hello JavaScript!</p>;
+const calc = document.getElementById('Calc');
+const paycheck = document.getElementById('Paycheck');
+const ideaOne = document.getElementById("ideaOne");
+const input = new URLSearchParams(window.location.search);
+
+//place to start posting information but will start as hidden until interaction/event
+ideaOne.hidden = true;
+
+//when calculate button is clicked, do this:
+calc.onclick = () => {
+    ideaOne.hidden = false;
+    ideaOne.insertAdjacentHTML("afterend", displayOne(input.get("Paycheck")));
+}
+//retrieve number from textbox to run function
+const displayOne = (income) => {
+    return `You make $${income}`;
+}
+
+// const paycheck = displayOne(input.get("Paycheck"));
